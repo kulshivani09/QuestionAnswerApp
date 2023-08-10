e996353f7891bcd7c44b9a2b40e38d9f89341685
kulshivani09 <shivanivkulkarni09@gamil.com>
2023-08-10 18:13:49 +0530

    Revert "All functionality included"
    This reverts commit f0349f9dafd865c6ebb6cc65cb560453cfc44b0d.

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from flask import Flask,render_template,request,flash,redirect,url_for,send_file
+from flask import Flask,render_template,request,flash,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 from datetime import date
 from flask_migrate import Migrate
 from flask_login import UserMixin,login_user,login_required,logout_user,current_user,LoginManager
-from werkzeug.utils import secure_filename
-import os
 
 app=Flask(__name__)
 app.secret_key = 'srpilusm'
@@ -63,25 +61,12 @@ class Answers(db.Model):
     ans_posted_date=db.Column(db.Date)
     posted_by=db.Column(db.String(600))
     ques_id=db.Column(db.Integer,db.ForeignKey('Questions.ques_id'),nullable=False)
-    files=db.relationship('Files',backref='answers',cascade='all, delete-orphan')
 
     def __init__(self,ans_desc,posted_by,ques_id):
         self.ans_desc=ans_desc
         self.ans_posted_date=date.today()
         self.posted_by=posted_by
         self.ques_id=ques_id
-
-class Files(db.Model):
-    __tablename__ ='Files'
-    file_id=db.Column(db.Integer,primary_key=True)
-    file_name=db.Column(db.String(70))
-    ans_id=db.Column(db.Integer,db.ForeignKey('Answers.ans_id'),nullable=False)
-
-
-    def __init__(self,file_name,ans_id):
-        self.file_name=file_name
-        self.ans_id=ans_id
-
 
 #login
 @app.route("/login",methods=['GET','POST'])
@@ -159,44 +144,21 @@ def answer_question():
             user_ans=request.form['user_ans']
             curr_user=current_user.email
             q_id=request.form['q_id']
-            # file = request.files["file"]
-            files = request.files.getlist("file")
-            print(files)
             answer=Answers(user_ans,curr_user,q_id)
             db.session.add(answer)
             db.session.commit()
-            db.session.refresh(answer)
-            if files:
-                for file in files:
-                    if file.filename=='':
-                        break
-                    file_upload=Files(file.filename,answer.ans_id)
-                    db.session.add(file_upload)
-                    db.session.commit()
-                    file.save(f'uploads/{secure_filename(file.filename)}')
             flash('Answer added successfully')
     except Exception as e:
         print(e)
     return redirect(url_for('homepage'))
 
 #View Answers
-@app.route("/answer/<int:question_id>",methods=['GET','POST'])
+@app.route("/answer/<int:ques_id>",methods=['GET','POST'])
 @login_required
-def view_answers(question_id):
-    answers = Answers.query.filter_by(ques_id=question_id).all()
-    questions=Questions.query.get(question_id)
-    file_dic={}
-    if answers:
-        for answer in answers:
-            print(answer.ans_id)
-            files=Files.query.filter_by(ans_id=answer.ans_id).all()
-            #file_dic[answer.ans_id].append(files)    
-            file_dic[answer.ans_id]=files
-        print(file_dic)
-        return render_template('answers.html',questions=questions,answers=answers,file_dic=file_dic)
-    else:
-        return render_template('answers.html',questions=questions,answers=answers)
-
+def view_answers(ques_id):
+    answers = Answers.query.filter_by(ques_id=ques_id).all()
+    questions=Questions.query.get(ques_id)
+    return render_template('answers.html',questions=questions,answers=answers)
 
 #Update Answer
 @app.route("/updateAnswer",methods=['GET','POST'])
@@ -214,7 +176,7 @@ def update_answer():
             flash('Answer Updated successfully')
     except Exception as e:
         print(e)
-    return redirect(url_for('view_answers',question_id=updated_answer.ques_id))
+    return redirect(url_for('view_answers',ques_id=updated_answer.ques_id))
     #return render_template('answer.html')
 
 #delete Answer
@@ -230,7 +192,7 @@ def delete_answer(ans_id):
             flash("Answer deleted successfully")
     except Exception as e:
         print(e)
-    return redirect(url_for('view_answers',question_id=ques_id))
+    return redirect(url_for('view_answers',ques_id=ques_id))
 
 #delete Question
 @app.route("/deleteQuestion/<int:ques_id>",methods=['GET','POST'])
@@ -246,13 +208,7 @@ def delete_question(ques_id):
         print(e)
     return redirect(url_for('homepage'))
 
-@app.route("/view_doc/<filename>",methods=['GET','POST'])
-@login_required
-def view_doc(filename):
-    doc_name=secure_filename(filename)
-    file_path = os.path.join(app.root_path, 'uploads', doc_name)
-    return send_file(file_path, mimetype='application/pdf')
-    
+
 
 #logout
 @app.route('/logout',methods=['GET','POST'])
